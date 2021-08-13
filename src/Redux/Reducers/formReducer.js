3e996c0df51b3ebd53f2cc65beea20eb1b31f5a5
ymoredeqode <ymore@deqode.com>
2021-08-13 11:45:57 +0530

    state fixes

--- src/Redux/Reducers/formReducer.js
+++ src/Redux/Reducers/formReducer.js
@@ -1,28 +1,30 @@
 import * as Constants  from '../../Constants/ActionKeys.js';
 
 // const initialState = [];
-const initialState = {};
+const initialState = {
+    user:[]
+};
 
 
 const contactReducer = (state = initialState, action) => {
     switch(action.type){
 
         case Constants.FETCH_USERS_SUCCESS : 
-        state = action.payload;
+        state.user = action.payload;
         return state;
 
         case Constants.ADD_USER : 
-        state = [... state, action.payload]; // use thi to push data into array
+        state = [... state.user, action.payload]; // use this to push data into array
         return state;
 
         case Constants.UPDATE_USER : 
-        const UpdateState = state.map((item)=>item.id === action.payload.id ? action.payload : item);
-        state = UpdateState;
+        const UpdateState = state.user.map((item)=>item.id === action.payload.id ? action.payload : item);
+        state.user = UpdateState;
         return state;
         
         case Constants.DELETE_USER : 
-        const deleteState = state.filter((item)=>item.id !== action.payload && item);
-        state = deleteState;
+        const deleteState = state.user.filter((item)=>item.id !== action.payload && item);
+        state.user = deleteState;
         return state;
 
         default:
